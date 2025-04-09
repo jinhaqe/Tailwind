9b794089c2c7af263f2ff894fa9ec19d24b2cadc
jinha <128437713+jinhaqe@users.noreply.github.com>
2025-04-09 16:37:02 +0900

    기본 셋팅

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+   return (
+      <>
+         <div className="h-screen flex flex-row justify-between items-center">
+            <div className="">투두</div>
+            <div>냠</div>
+         </div>
+      </>
+   );
+}
+
+export default App;

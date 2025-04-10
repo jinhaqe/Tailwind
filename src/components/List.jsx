03619e7bcfa063cb241e8be6912866c055192c56
jinha <128437713+jinhaqe@users.noreply.github.com>
2025-04-10 13:20:06 +0900

    사이즈 변경

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,7 +2,7 @@ import DetailList from "./DetailList.jsx";
 
 export default function List({ setPage, list, setDetail }) {
    return (
-      <div className="w-[20%] h-[97%] bg-sky-950 rounded-tr-3xl rounded-br-3xl flex flex-col justify-start items-left pt-10 px-6">
+      <div className="w-[15%] h-[97%] bg-sky-950 rounded-tr-3xl rounded-br-3xl flex flex-col justify-start items-left pt-10 px-6">
          <p className="text-xl font-bold text-white">YOUR PROJECTS</p>
          <button
             className="mt-9 px-6 py-2 border-2 w-[140px] border-white text-white font-semibold rounded-lg opacity-75 hover:bg-sky-900 transition "
